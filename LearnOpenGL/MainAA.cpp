fb0039f10875e99efacdafc5fa5c630f0cc38934
Teasue <296609391@qq.com>
2022-12-15 17:37:49 +0800

    FXAA

---
+++ LearnOpenGL/MainAA.cpp
@@ -0,0 +1,306 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "stb_image.h"
+#include <iostream>
+#include "camera.h"
+#include "model.h"
+#include "shader.h"
+using namespace std;
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+unsigned int loadTexture(char const* path);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+float deltaTime, lastTime = 0.0f;
+float lastX = SCR_WIDTH / 2, lastY = SCR_HEIGHT / 2;
+bool firstMouse = true;
+
+Camera camera(glm::vec3(1.0f, 5.0f, -22.0f));
+
+glm::vec3 lightPos(1.2f, 10.0f, -10.0f);
+
+int main()
+{
+    // glfw: initialize and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_SAMPLES, 4);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //glEnable(GL_MULTISAMPLE);
+
+    // Setup and compile our shaders
+    Shader shader("light.vert", "light.frag");
+    Shader screenShader("vs_framebuffer.vert", "fs_fxaa.frag");
+
+#pragma region "object_initialization"
+    // Set the object data (buffers, vertex attributes)
+    GLfloat cubeVertices[] = {
+        // Positions       
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f
+    };
+    float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
+    };
+    // Setup cube VAO
+    GLuint cubeVAO, cubeVBO;
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &cubeVBO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glBindVertexArray(0);
+
+    //quad
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+#pragma endregion
+
+    unsigned int framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+    unsigned int textureColorbuffer;
+    glGenTextures(1, &textureColorbuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT); // use a single renderbuffer object for both a depth AND stencil buffer.
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    screenShader.use();
+    screenShader.setInt("screenTexture", 0);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Set frame time
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastTime;
+        lastTime = currentFrame;
+
+        // Check and call events
+        processInput(window);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+        // Clear buffers
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+        // Set transformation matrices		
+        shader.use();
+        glm::mat4 projection = glm::perspective(camera.Fov, (GLfloat)SCR_WIDTH / (GLfloat)SCR_HEIGHT, 0.1f, 1000.0f);
+        glUniformMatrix4fv(glGetUniformLocation(shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(glGetUniformLocation(shader.ID, "view"), 1, GL_FALSE, glm::value_ptr(camera.GetViewMatirx()));
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::scale(model, glm::vec3(8.0f, 8.0f, 8.0f));
+        shader.setMat4("model", model);
+
+        glBindVertexArray(cubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        screenShader.use();
+        glBindVertexArray(quadVAO);
+        glBindTexture(GL_TEXTURE_2D, textureColorbuffer);	// use the color attachment texture as the texture of the quad plane
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // Swap the buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    float cameraSpeed = 2.5f * deltaTime; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.KeyBoradCallBack(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.KeyBoradCallBack(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.KeyBoradCallBack(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.KeyBoradCallBack(RIGHT, deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
+        cout << "CAMERA POSITION: " + to_string(camera.Position.x) + "      " + to_string(camera.Position.y) + "      " + to_string(camera.Position.z);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.MouseMoveCallBack(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.MouseSrollCallBack(yoffset);
+}
+
+unsigned int loadTexture(char const* path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
+}
---
+++ LearnOpenGL/fs_fxaa.frag
@@ -0,0 +1,123 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D screenTexture;
+
+float _MinThreshold = 0.0312;
+float _Threshold = 0.125;
+
+float GetLuma(vec3 color)
+{
+    return color.x * 0.213 + color.y * 0.715 + color.z * 0.072;
+};
+
+void main()
+{ 
+    vec2 texsize = textureSize(screenTexture, 0);
+    vec2 uvstep = vec2(1.0 / texsize.x, 1.0 / texsize.y);
+
+    float N = GetLuma(texture(screenTexture, TexCoords + uvstep * vec2(0.0, 1.0)).rgb);
+    float S = GetLuma(texture(screenTexture, TexCoords + uvstep * vec2(0.0, -1.0)).rgb);
+    float E = GetLuma(texture(screenTexture, TexCoords + uvstep * vec2(1.0, 0.0)).rgb);
+    float W = GetLuma(texture(screenTexture, TexCoords + uvstep * vec2(-1.0, 0.0)).rgb);
+    
+    float M = GetLuma(texture(screenTexture, TexCoords).rgb);
+
+    float MaxLuma = max(M, max(max(N, S), max(E, W)));
+    float MinLuma = min(M, min(min(N, S), min(E, W)));
+
+    float Contrast = MaxLuma - MinLuma;
+    if (Contrast >= max(_MinThreshold, MaxLuma * _Threshold))
+    {
+        float NW = GetLuma(texture(screenTexture, TexCoords + uvstep * vec2(-1.0, 1.0)).rgb);
+        float SW = GetLuma(texture(screenTexture, TexCoords + uvstep * vec2(-1.0, -1.0)).rgb);
+        float SE = GetLuma(texture(screenTexture, TexCoords + uvstep * vec2(1.0, -1.0)).rgb);
+        float NE = GetLuma(texture(screenTexture, TexCoords + uvstep * vec2(1.0, 1.0)).rgb);
+
+        float Filter = 2 * (N + E + S + W) + NE + NW + SE + SW;
+        Filter = Filter / 12;
+        Filter = abs(Filter -  M);
+        Filter = min(Filter / Contrast, 1);
+        float PixelBlend = smoothstep(0, 1, Filter);
+        PixelBlend = PixelBlend * PixelBlend;
+
+        float Vertical = abs(N + S - 2 * M) * 2+ abs(NE + SE - 2 * E) + abs(NW + SW - 2 * W);
+        float Horizontal = abs(E + W - 2 * M) * 2 + abs(NE + NW - 2 * N) + abs(SE + SW - 2 * S);
+        bool IsHorizontal = Vertical > Horizontal;
+        vec2 PixelStep = IsHorizontal ? vec2(0, uvstep.y) : vec2(uvstep.x, 0);
+        vec2 EdgeStep = IsHorizontal ? vec2(uvstep.x, 0) : vec2(0, uvstep.y);
+
+        float Positive = abs((IsHorizontal ? N : E) - M);
+        float Negative = abs((IsHorizontal ? S : W) - M);
+        bool IsNegative = Positive < Negative;
+        if (IsNegative) 
+            PixelStep = -PixelStep;
+
+
+        float OppositeM;
+        if (IsHorizontal)
+            OppositeM = IsNegative ? S : N;
+        else
+            OppositeM = IsNegative ? W : E;
+
+        float EdgeLuma = (M + OppositeM) * 0.5;
+        vec2 EdgePixel = TexCoords + PixelStep * 0.5;
+        float NextPixelLumaDelta, EdgePositiveDistance, EdgeNegativeDistance;
+        int i;
+        for (i = 1; i <= 10; i++)
+        {
+            NextPixelLumaDelta = GetLuma(texture(screenTexture, EdgePixel + i * EdgeStep).rgb) - EdgeLuma;
+            if (abs(NextPixelLumaDelta) > EdgeLuma * 0.25)
+            {
+                EdgePositiveDistance = i * (IsHorizontal ? EdgeStep.x : EdgeStep.y);
+                break;
+            }
+        }
+
+        if (i == 11)
+        {
+            EdgePositiveDistance = 10 * (IsHorizontal ? EdgeStep.x : EdgeStep.y);
+        }
+
+        for (i = 1; i <= 10; i++)
+        {
+             NextPixelLumaDelta = GetLuma(texture(screenTexture, EdgePixel - i * EdgeStep).rgb) - EdgeLuma;
+            if (abs(NextPixelLumaDelta) > EdgeLuma * 0.25)
+            {
+                EdgeNegativeDistance = i * (IsHorizontal ? EdgeStep.x : EdgeStep.y);
+                break;
+            }
+        }
+
+        if (i == 11)
+        {
+            EdgeNegativeDistance = 10 * (IsHorizontal ? EdgeStep.x : EdgeStep.y);
+        }
+
+        float EdgeBlend;
+        if (EdgePositiveDistance < EdgeNegativeDistance)
+        {
+            if (sign(NextPixelLumaDelta) == sign(M - EdgeLuma))
+                EdgeBlend = 0;
+            else
+                EdgeBlend = 0.5 - EdgePositiveDistance / (EdgePositiveDistance + EdgeNegativeDistance);
+        }
+        else
+        {
+            if (sign(NextPixelLumaDelta) == sign(M - EdgeLuma))
+                EdgeBlend = 0;
+            else
+                EdgeBlend = 0.5 - EdgeNegativeDistance / (EdgePositiveDistance + EdgeNegativeDistance);
+        }
+
+        float FinalBlend = max(PixelBlend, EdgeBlend);
+
+        FragColor = texture(screenTexture, TexCoords + PixelStep * FinalBlend);
+    }
+    else
+    {
+        FragColor = texture(screenTexture, TexCoords);
+    }
+}
